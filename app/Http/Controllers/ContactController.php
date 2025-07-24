7a03a441aab4da274f2b4229d1603e7c0a1b7217
fatimazahra190 <136029317+fatimazahra190@users.noreply.github.com>
2025-07-24 11:37:14 +0100

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -38,7 +38,7 @@ class ContactController extends Controller
 
         Contact::create($validated);
 
-        return redirect()->route('contact')->with('success', 'Thank you for your message. We will get back to you soon!');
+        return redirect()->route('contact.index')->with('success', 'Thank you for your message. We will get back to you soon!');
     }
 
     /**
